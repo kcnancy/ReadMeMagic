76e4b5071ce900d4d0e129141f1d915da68e3485
Nancy McBride <njmcbride224@gmail.com>
2021-02-13 09:53:36 -0600

    updating index file to use utils folder, adding utils folder

--- index.js
+++ index.js
@@ -1,54 +1,84 @@
-const inquirer = require("inquirer")
+
+// TODO: Include packages needed for this application
+
+const inquirer = require("inquirer");
 const fs = require("fs");
-fs.writeFile(Readme.md, data) {}
+const util = require ("util");
+const generateReadme = require("./utils/generateReadme");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+// TODO: Create an array of questions for user input
+
+const questions = [
 
 inquirer
 .prompt([
     {
-        name: "GitHubname",
+        name: "username",
         type: "input",
-        message: "What is your GitHub username?",
+        message: "What is your GitHub username? ",
 },
 {
-    name: "email",
-    type: "input",
-    message: "What is your email address?",
+        name: "email",
+        type: "input",
+        message: "What is your email address? ",
 },
     {
         name: "project_title",
         type: "input",
-        message: "What is the title of your project?",
+        message: "What is the title of your project? ",
 },
     {
-    name: "description",
-    type: "input",
-    message: "Describe your project,",
+        name: "description",
+        type: "input",
+        message: "Describe your project: ",
+},
+{
+        name: "installation",
+        type: "input",
+        message: "Describe the installation process if any: ",
 },
     {
-    name: "usage",
-    type: "input",
-    message: "Provide instructions and examples for use,",
+        name: "usage",
+        type: "input",
+        message: "Provide instructions and examples for use:",
 },
     {
-    name: "contribution",
-    type: "input",
-    message: "Provide contribution guidelines,",
+        name: "contribution",
+        type: "input",
+        message: "Provide contribution guidelines:",
 },
     {
-    name: "test",
-    type: "input",
-    message: "Provide test instructions,",
+        name: "test",
+        type: "input",
+        message: "Provide test instructions:",
 },
     {
-    name: "license",
-    type: "list",
-    message: "Choose the license,",
-    choices: ["MIT", "Bower", "None" ]
+        name: "license",
+        type: "list",
+        message: "Choose the license,",
+        choices: ["MIT", "Apache", "Academic", "GNU", "ISC", "Mozilla", "Open", "None" ]
 },
 ])
-//..console log to check my questions
-.then ((answer) => {
-console.log("My Project is" + answer.project_title);
-console.log(answer.GitHubname);
-console.log(answer.email)
-});
+];
+
+
+
+// TODO: Create a function to write README file
+
+const writeToFile = (fileName, data) => {
+    fs.writeToFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log(success)
+        );
+    
+ // Write new README.md to output directory
+
+        fs.writeFile('./output/README.md', generateReadme(data));
+        console.log('Successfully wrote to README.md');
+       
+    }
+  
+
+// Function call to initialize app
+init();
